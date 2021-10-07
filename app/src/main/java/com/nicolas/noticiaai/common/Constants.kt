961dfeef5bc3d6236a9dd693e99f4cc83243a0fc
Aleixo-Dev <nicolasaleixo2020@gmail.com>
2021-10-07 15:09:08 -0300

    constants base

---
+++ app/src/main/java/com/nicolas/noticiaai/common/Constants.kt
@@ -0,0 +1,15 @@
+package com.nicolas.noticiaai.common
+
+object Constants {
+
+    const val APP_ENDPOINT = "top-headlines"
+    const val BASE_URL = "https://newsapi.org/v2/"
+    const val COUNTRY_BR = "br"
+    const val CATEGORY_SPORTS = "sports"
+    const val CATEGORY_ENTERTAINMENT = "entertainment"
+    const val CATEGORY_HEALTH = "health"
+    const val CATEGORY_SCIENCE = "science"
+    const val CATEGORY_BUSINESS = "business"
+    const val CATEGORY_TECHNOLOGY = "technology"
+
+}
